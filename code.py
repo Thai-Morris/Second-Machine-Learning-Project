ddb86f716079ec5963fe742206a185a2412564fb
Thai Morris <30935442+Thai-Morris@users.noreply.github.com>
2019-01-14 21:43:58 -0500

    Grabbed data, Summarized it, and added visuals

--- code.py
+++ code.py
@@ -5,4 +5,20 @@ from skylearn.metrics import accuracy_score
 
 '''Load training dataset'''
 
+url = "https://raw.githubusercontent.com/callxpert/datasets/master/data-scientist-salaries.cc"
+names = ['Years-experience', 'Salary']
+dataset = pandas.read_csv(url, names=names)
+
+''' shape '''
+print(dataset.shape)
+
+'''visualize'''
+
+dataset.plot()
+plt.show()
+
+'''Since my dataset is small, I will use nine records for training the model and 1 record to evaulute the model. Copy paste the below commands to prepare my datasets.'''
+
+X = dataset[['Years-exeperience']]
+y = dataset['Salary']
 
