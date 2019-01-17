224d159f4eb03090657efef1486226a57a0d9e23
Thai Morris <30935442+Thai-Morris@users.noreply.github.com>
2019-01-16 23:49:50 -0500

    added the last bits of code

--- code.py
+++ code.py
@@ -21,4 +21,14 @@ plt.show()
 
 X = dataset[['Years-exeperience']]
 y = dataset['Salary']
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=101)
 
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(X_train,y_train)
+
+predictions = model.predict(X_test)
+print(accuracy_score(y_test,predictions))
+
+print(model.predict(6.3))
